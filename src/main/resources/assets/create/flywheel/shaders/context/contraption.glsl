63c417356a13e84dce1332bc5fae52be57b2bf95
Tropheus Jay <jverrellijr@icloud.com>
2021-10-24 13:46:05 -0400

    LOOP FIXED

---
+++ src/main/resources/assets/create/flywheel/shaders/context/contraption.glsl
@@ -0,0 +1,68 @@
+#use "flywheel:context/fog.glsl"
+
+out vec3 BoxCoord;
+out vec4 FragmentColor;
+
+uniform sampler3D uLightVolume;
+
+uniform sampler2D uBlockAtlas;
+uniform sampler2D uLightMap;
+
+uniform vec3 uLightBoxSize;
+uniform vec3 uLightBoxMin;
+uniform mat4 uModel;
+
+uniform float uTime;
+uniform mat4 uViewProjection;
+uniform vec3 uCameraPos;
+
+uniform vec2 uWindowSize;
+
+void FLWFinalizeNormal(inout vec3 normal) {
+    mat3 m;
+    m[0] = uModel[0].xyz;
+    m[1] = uModel[1].xyz;
+    m[2] = uModel[2].xyz;
+    normal = m * normal;
+}
+
+#if defined(VERTEX_SHADER)
+
+void FLWFinalizeWorldPos(inout vec4 worldPos) {
+    worldPos = uModel * worldPos;
+
+    BoxCoord = (worldPos.xyz - uLightBoxMin) / uLightBoxSize;
+
+    #if defined(USE_FOG)
+    FragDistance = length(worldPos.xyz);
+    #endif
+
+    gl_Position = uViewProjection * worldPos;
+}
+
+#elif defined(FRAGMENT_SHADER)
+#use "flywheel:core/lightutil.glsl"
+
+vec4 FLWBlockTexture(vec2 texCoords) {
+    return texture2D(uBlockAtlas, texCoords);
+}
+
+void FLWFinalizeColor(vec4 color) {
+    #if defined(USE_FOG)
+    float a = color.a;
+    float fog = clamp(FLWFogFactor(), 0., 1.);
+
+    color = mix(uFogColor, color, fog);
+    color.a = a;
+    #endif
+
+    FragmentColor = color;
+}
+
+vec4 FLWLight(vec2 lightCoords) {
+    lightCoords = max(lightCoords, texture(uLightVolume, BoxCoord).rg);
+
+    return texture(uLightMap, shiftLight(lightCoords));
+}
+
+#endif
