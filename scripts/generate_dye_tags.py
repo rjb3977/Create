32b1afa09bdfec197d58cb3bada0f41dfc5be3ef
Tropheus Jay <jverrellijr@icloud.com>
2021-10-30 16:02:07 -0400

    another script, dye tags done

---
+++ scripts/generate_dye_tags.py
@@ -0,0 +1,20 @@
+import os
+colors = {"white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown", "green", "red", "black"}
+tagsPath = "../src/main/resources/data/c/tags/items/dyes/"
+tagTemplate = '''{
+  "replace": false,
+  "values": [
+    "minecraft:%s_dye"
+  ]
+}
+'''
+
+for color in colors:
+    tagName = tagsPath + color + ".json"
+    if not os.path.exists(tagsPath):
+        os.makedirs(tagsPath)
+    with open(tagName, "w") as tagFile:
+        # create parent directories
+        tagFile.write(tagTemplate % color)
+        tagFile.close()
+    print(tagName)
