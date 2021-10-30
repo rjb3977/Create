8dd32de4550cb62b750e26c93e8acaecab97a96e
Tropheus Jay <jverrellijr@icloud.com>
2021-10-30 15:10:23 -0400

    ran script

--- scripts/remove_compat_recipes.py
+++ scripts/remove_compat_recipes.py
@@ -2,11 +2,20 @@ import os
 
 dataDir = "../src/generated/resources/data/"
 mods = {"silents_mechanisms", "immersiveengineering", "mekanism", "eidolon", "mysticalworld", "thermal", "iceandfire"}
+toMove = []
 
-# searches src/generated/resources/data/ for all files containing a mod name and moves them to src/generated/resources/compat_recipes
-def remove():
-    for fileName in os.listdir(dataDir):
-        for modName in mods:
-            if fileName.endswith(modName + ".json"):
-                os.rename(dataDir + fileName, "../src/generated/resources/compat_recipes/" + fileName)
-                break
+def iterateDir(dir):
+    for fileName in os.listdir(dir):
+        if os.path.isdir(dir + fileName):
+            iterateDir(dir + fileName + "/")
+        else:
+            for modName in mods:
+               if fileName.endswith("compat_" + modName + ".json"):
+                   toMove.append(dir + fileName)
+
+iterateDir(dataDir)
+for fileName in toMove:
+    newName = fileName.replace(dataDir, "../src/generated/resources/compat_recipes/")
+    os.makedirs(os.path.dirname(newName), exist_ok=True)
+    os.rename(fileName, newName)
+    print("Moved " + fileName + " to " + newName)
